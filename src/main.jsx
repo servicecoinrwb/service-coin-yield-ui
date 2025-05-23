b91230b41ab4e79db09bb85ab543386b5603999d
Service <info@service.money>
2025-05-23 11:31:32 -0400

    Fix ClaimRewards import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ClaimRewards from './ClaimRewards';
+import ClaimRewards from './components/ClaimRewards';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<ClaimRewards />);
