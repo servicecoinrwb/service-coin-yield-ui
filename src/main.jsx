29aa5d03a8918294358fb7afca24ade4705197a0
Service <info@service.money>
2025-05-23 13:31:14 -0400

    Connect ClaimRewards to main UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import ClaimRewards from './ClaimRewards';
+import ClaimRewards from './components/ClaimRewards';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<ClaimRewards />);
+function App() {
+  return <ClaimRewards />;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
